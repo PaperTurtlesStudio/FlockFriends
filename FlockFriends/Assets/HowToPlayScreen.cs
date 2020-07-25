2aa830bc41d5ca5f66c69378a34198ae70a6c58d
PaperTurtlesStudio <66807368+PaperTurtlesStudio@users.noreply.github.com>
2020-07-25 14:57:21 -0700

    HowToPlay Screen

---
+++ FlockFriends/Assets/HowToPlayScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToPlayScreen : MonoBehaviour
+{
+    public GameObject HowTo;
+    public GameObject BreadText;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(DeactitivateScreen());
+    }
+
+    IEnumerator DeactitivateScreen()
+    {
+        yield return new WaitForSeconds(7f);
+
+        HowTo.SetActive(false);
+        BreadText.SetActive(true);
+    }
+}
