32830a98f1c5af41f5054db433416a8b94ea84d3
PaperTurtlesStudio <66807368+PaperTurtlesStudio@users.noreply.github.com>
2020-07-26 14:55:26 -0700

    Turts Anims

---
+++ FlockFriends/Assets/Scripts/Managers/AnimationManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    public Animator PengoAnimator;
+    public Animator OstarAnimator;
+    public Animator TurtsAnimator;
+
+    private void Start()
+    {
+        for (int i = 0; i < ItemManager.TurtsWearable.Count; i++)
+        {
+            if (ItemManager.TurtsWearable[i].wearing)
+            {
+                TurtsAnimator.SetBool(i.ToString(), true);
+            }
+            else
+            {
+                TurtsAnimator.SetBool(i.ToString(), false);
+            }
+        }
+    }
+
+}
